bb50050457ec6cceb902a71e073fa3a93f262e41
Alvaro Campos Ferreira <alvaro.campos.ferreira@gmail.com>
2018-07-21 00:54:34 -0300

    Implement A,b matrices for H-matrices and approximation by interpolation.

--- src/const3D_tri/cal_Aeb.jl
+++ src/const3D_tri/cal_Aeb.jl
@@ -1,63 +1,65 @@
 function cal_Aeb(b1,b2,arg)
-    # Builds the matrices for the linear system A x = b
-    NOS,NOS_GEO,ELEM,k,qsi,w,inc,CDC = arg
-    #NOS,NOS_GEO,ELEM,fc,qsi,w,CDC,k=arg
-    nelem::Int64=size(ELEM)[1]; # Number of elements
-    qsitelles,Jtelles = telles(qsi,0); # Evaluating the Telles' points and its Jacobian
-    npg=4; # Number of integration points
-    qsiquad,wquad = Gauss_Legendre(-1,1,npg) # Generation of the points and weights
-    B=complex(zeros(length(b1),length(b2)));	# Allocates matrix B
-    A=complex(zeros(length(b1),length(b2)));	# Allocates matrix A
-    q=zeros(length(b1),1);	# Allocates array q
+    NOS, NOS_GEO, ELEM, k, CDC,qsi,w,qsi_tri,w_tri = arg;
+    nnos = length(b1);
+    nelem = length(b2)
+    G=zeros(nnos,nelem); 
+    H=zeros(nnos,nelem);     
+    A = complex(zeros(nnos,nelem));
+    b = complex(zeros(nnos, 1));
     ci=0
-    for i in b1 # Loop over the source points
+    for i in b1
         ci+=1
-        xd=NOS[i,2]; # x coordinate of the source point
-        yd=NOS[i,3]; # y coordinate of the source point
-        zd=NOS[i,3]; # y coordinate of the source point
-
+        xd=NOS[i,2]; 
+        yd=NOS[i,3]; 
+        zd=NOS[i,4];
+        
         cj=0
-        for j in b2 # Loop over the elements
+        for j in b2
             cj+=1
-            no1=ELEM[j,2]; # Primeiro n� geom�trico do elemento
-            no2=ELEM[j,3]; # Segundo n� geom�trico do elemento
-            no3=ELEM[j,4]; # Terceiro n� geom�trico do elemento
+	    tipoCDC = CDC[j,2]; 
+	    
+	    valorCDC = CDC[j,3];
+	    nos = ELEM[j,2:4];		
 
-            x1=NOS_GEO[no1,2]; # Coordenada x do n� geom�trico 1
-            y1=NOS_GEO[no1,3]; # Coordenada y do n� geom�trico 1
-            z1=NOS_GEO[no1,4]; # Coordenada z do n� geom�trico 1
+            no1=ELEM[j,2]; 
+            no2=ELEM[j,3]; 
+            no3=ELEM[j,4]; 
 
-            x2=NOS_GEO[no2,2]; # Coordenada x do n� geom�trico 2
-            y2=NOS_GEO[no2,3]; # Coordenada y do n� geom�trico 2
-            z2=NOS_GEO[no2,4]; # Coordenada x do n� geom�trico 2
+            x1=NOS_GEO[no1,2]; 
+            y1=NOS_GEO[no1,3]; 
+            z1=NOS_GEO[no1,4]; 
 
-            x3=NOS_GEO[no3,2]; # Coordenada x do n� geom�trico 3
-            y3=NOS_GEO[no3,3]; # Coordenada y do n� geom�trico 3
-            z3=NOS_GEO[no3,4]; # Coordenada z do n� geom�trico 3
+            x2=NOS_GEO[no2,2]; 
+            y2=NOS_GEO[no2,3]; 
+            z2=NOS_GEO[no2,4]; 
 
-            n = calc_vetnormal(x1,y1,z1,x2,y2,z2,x3,y3,z3); # vetor unit�rio normal ao elemento
-            if i==j # The source point belongs to the element
-                #g,h = calcula_GeHs(x1,y1,x2,y2,1,k);	# Singular integration
-	        #g,h = calcula_GeHns(x1,y1,x2,y2,xd,yd,qsitelles,w.*Jtelles,k);	# Singular integration using the Telles transformation
-	        #h = 0.5;
-                g,h= calcula_HeGs(x1,y1,z1,x2,y2,z2,x3,y3,z3,xd,yd,zd,qsiquad,wquad,k)# Integra��o singular
+            x3=NOS_GEO[no3,2]; 
+            y3=NOS_GEO[no3,3]; 
+            z3=NOS_GEO[no3,4]; 
 
-                # println("Diferença entre g e gtelles = ", abs(g-gtelles))
-                # println("Diferença entre h e htelles = ", abs(h-htelles))
-            else # O ponto fonte n�o pertence ao elemento
-                g,h=calcula_GeHns(x1,y1,z1,x2,y2,z2,x3,y3,z3,xd,yd,zd,n,qsi,w,k); # Integra��o singular
-                #g,h = calcula_GeHns(x1,y1,x2,y2,xd,yd,qsi,w,k); # Non singular integration
-            end
-	    # Applying the boundary conditions
-            if CDC[j,2]==0	# The velocity potential is known
-                B[ci,cj] = -h	# Matrix B receives the value from matrix h
-                A[ci,cj] = -g	# Matrix A receives the value from matrix g
+            n = calc_vetnormal(x1,y1,z1,x2,y2,z2,x3,y3,z3); 
+            
+            if i==j 
+                g,h=calcula_HeGs_POT(x1,y1,z1,x2,y2,z2,x3,y3,z3,xd,yd,zd,qsi,w,k); 
+            else 
+                g,h=calcula_HeGns_POT(x1,y1,z1,x2,y2,z2,x3,y3,z3,xd,yd,zd,n,qsi_tri,w_tri,k); 
+            end            
+            if tipoCDC == 0 
+        	A[ci,cj] = -g; 	
+                if valorCDC == 0
+                    b[ci,1] = b[ci,1] - 0; 
+                else
+                    b[ci,1] = b[ci,1] - h*valorCDC; 
+                end                
             else
-                B[ci,cj] = g	# Matrix B receives the value from matrix g
-                A[ci,cj] = h	# Matrix A receives the value from matrix h
+                A[ci,cj] = +h; 	
+                if valorCDC == 0
+                    b[ci,1] = b[ci,1] + 0; 
+                else
+                    b[ci,1] = b[ci,1] + g*valorCDC; 
+                end
             end
         end
     end
-    b = B*(CDC[b2,3])  # Builds the b array for the linear system
     return A,b
 end
