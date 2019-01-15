56200b77453d7ce3cd374176f9a39d373ecaef60
Álvaro Cafe <alvaro.campos.ferreira@gmail.com>
2019-01-15 19:11:45 -0200

    Making unit test files

---
+++ tests/runtests.jl
@@ -0,0 +1,29 @@
+@testset "2D" begin
+    @testset "Wave equation" begin
+        @testset "Interior problems" begin
+        end
+        @testset "Exterior problems" begin
+        end
+    end
+    @testset "Potential equation" begin
+        @testset "Interior problems" begin
+        end
+        @testset "Exterior problems" begin
+        end
+    end
+end
+
+@testset "3D" begin
+    @testset "Wave equation" begin
+        @testset "Interior problems" begin
+        end
+        @testset "Exterior problems" begin
+        end
+    end
+    @testset "Potential equation" begin
+        @testset "Interior problems" begin
+        end
+        @testset "Exterior problems" begin
+        end
+    end
+end
