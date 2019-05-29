1cdf4d75ee7a05eadf65b22db0a51d0eff26e0ca
Álvaro Cafe <alvaro.campos.ferreira@gmail.com>
2019-05-29 17:04:02 -0300

    Separating convengernce for VT from const3D.

--- tests/VT_convergence.jl
+++ tests/VT_convergence.jl
@@ -5,45 +5,18 @@
 #The main function is const2D.solve() which builds the influence matrices,
 #applies the boundary conditions, solves the linear system and returns the
 #value of the potential and its gradient at boundary and domain points.
-#module const3D
 using KrylovMethods, Statistics, LinearAlgebra, DelimitedFiles, PyCall, JLD
-include("dad.jl")
-include("hmat.jl")
-include("bem_functions.jl")
-include("lermsh.jl")
-include("tree.jl")
+filed = "../src/waveconst3d/"
+include(string(filed,"dad.jl"))
+include(string(filed,"hmat.jl"))
+include(string(filed,"bem_functions.jl"))
+include(string(filed,"lermsh.jl"))
+include(string(filed,"tree.jl"))
 # Gaussian quadrature - generation of points and weights [-1,1]
 npg=4; # Number of integration points
 qsi,w = Gauss_Legendre(-1,1,npg) # Generation of the points and weights
 
-function solve(info,PONTOS_int,fc,BCFace,k)
-    ## CBIE - Conventional Boundary Integral Equation
-    NOS_GEO,NOS,ELEM,CDC = info
-    nnos = size(NOS,1)  # Number of physical nodes, same as elements
-    b1 = 1:nnos # Array containing all the indexes for nodes and elements
-    npg=6
-    qsi,w = Gauss_Legendre(-1,1,npg) # Generation of the points and weights
-    A,b = cal_Aeb(b1,b1, [NOS,NOS_GEO,ELEM,fc,qsi,w,CDC,k])
-    x = A\b # Solves the linear system
-    phi,qphi = monta_phieq(CDC,x) # Applies the boundary conditions
-    phi_dom = calc_phi_pint(PONTOS_int,NOS_GEO,ELEM,phi,qphi,fc,fc,qsi,w,k)
-    return phi, qphi, phi_dom, phi_dom
-end
-function solveH(info,PONTOS_int,fc,BCFace,k)
-    ## H-Matrix BEM - Interpolation using Lagrange polynomial
-    NOS_GEO,NOS,ELEM,CDC = info
-    Tree,block = cluster(NOS[:,2:3],floor(sqrt(length(NOS))),2)
-    npg=6
-    qsi,w = Gauss_Legendre(-1,1,npg) # Generation of the points and weights
-    A,b = Hinterp(Tree,block,[NOS,NOS_GEO,ELEM,fc,qsi,w,CDC,k])
-    vet->matvec(A,vet,block,Tree)
-    x = gmres(vet,b)
-    phi,qphi = monta_phieq(CDC,x[1]) # Applies the boundary conditions
-    phi_dom = calc_phi_pint(PONTOS_int,NOS_GEO,ELEM,phi,qphi,fc,fc,qsi,w,k)
-    return phi,qphi,phi_dom,phi_dom
-end
-
-files = ["VT_A_coarse.msh" "VT_A_coarsest.msh" "VT_A_fine.msh" "VT_A_very_fine.msh" "VT_A.msh"]
+files = [ "VT_A_coarsest.msh" "VT_A_coarse.msh" "VT_A_fine.msh"]
 # Python - mesh.io
 meshio = pyimport("meshio")
 # Build the domain points
@@ -61,14 +34,23 @@ BCFace = ones(30,3);
 BCFace[:,3] .= 0;
 BCFace[1,:] = [1 0 1]; # Dirichlet (pressure = 1) to the Glotis
 BCFace[30,:] = [30 0 0]; # Dirichlet (pressure = 0) to the mouth
-mshd = "tests/data/"
+mshd = "./tests/data/"
 t = []
 for i in files
     mesh = meshio.read(string(mshd,i))
     NOS_GEO = [1:size(mesh.points,1) mesh.points]
-    ELEM = [1:size(mesh.cells["triangle"],1) mesh.cells["triangle"].+1 mesh.cell_data["triangle"]["gmsh:geometrical"]]
+    nelem = size(mesh.cells["triangle"],1)
+    ELEM = [1:nelem mesh.cells["triangle"].+1 mesh.cell_data["triangle"]["gmsh:geometrical"]]
     CDC,NOS = gera_vars(ELEM,BCFace,NOS_GEO)
-    FR = 1;CW = 1;inc = [0]
+    FR = 1;CW = 1;inc = 0
+    println(i," nelem = ",nelem)
+    # Conventional BEM
+    b1 = 1:nelem; b2 = 1:nelem;
+    tmatrixc = @elapsed Ac,bc = cal_Aeb(b1,b2,[NOS,NOS_GEO,ELEM,qsi,w,CDC,FR,CW])
+    tsolvec = @elapsed xc = bc\Ac
+    Tc,qc = monta_Teq(CDC,xc)
+    T_pintc = calc_T_pint(PONTOS_int,NOS_GEO,ELEM,Tc,qc,FR,CW,qsi,w,inc)
+    println("tmatrixc = ", tmatrixc,", tsolvec = ",tsolvec)
     # H-BEM
     # max_elem = Define máximo de nós em cada folha, tal que: max_elem/2 <= nós em cada folha < max_elem
     # max_elem=floor(sqrt(2*length(NOS[:,1])))
@@ -84,6 +66,7 @@ for i in files
     tsolve = @elapsed xi = gmres(vet->matvec(hmati,vet,block,Tree),bi,5,tol=1e-8,maxIter=1000,out=0) 
     T,q=monta_Teq(CDC,xi[1])
     T_pint = calc_T_pint(PONTOS_int,NOS_GEO,ELEM,T,q,FR,CW,qsi,w,inc)
-    save(string(mshd,i,".jld"),"ttree",ttree,"tmatrix",tmatrix,"tsolve","tsolve")
+    println("ttree = ",ttree,", tmatrix = ", tmatrix,", tsolve = ",tsolve)    
+    global t = append!(t,[ttree tmatrix tsolve tmatrixc tsolvec])
+    #save(string(mshd,i,".jld"),"ttree",ttree,"tmatrix",tmatrix,"tsolve","tsolve")
 end # files for
-#end # module const3D
